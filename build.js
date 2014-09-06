6d8c81949d8eddf5da00a911461080c967f6c487
Nathan Dinsmore <nfdins@gmail.com>
2014-09-06 17:19:22 -0400

    Remove some copy-pasted comments

--- build.js
+++ build.js
@@ -1,15 +1,13 @@
 var NwBuilder = require('node-webkit-builder');
 var nw = new NwBuilder({
-  files: './app.nw/**/**', // use the glob format
+  files: './app.nw/**/**',
   platforms: ['win', 'osx'],
   macIcns: 'resources/Elm.icns',
   winIco: 'resources/Elm.ico'
 });
 
-// Log stuff you want
 nw.on('log',  console.log);
 
-// Build returns a promise
 nw.build().catch(function(err) {
   console.error(err);
 });
