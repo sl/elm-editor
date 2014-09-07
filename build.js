6f2b4c09dbc20d0900bd01fb39532b9a9df92218
Nathan Dinsmore <nfdins@gmail.com>
2014-09-07 16:28:07 -0400

    Automatically build zips on OS X

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var exec = require('child_process').exec;
 var NwBuilder = require('node-webkit-builder');
 
 var nw = new NwBuilder({
@@ -13,6 +14,10 @@ nw.on('log',  console.log);
 
 nw.build().then(function() {
   fs.createReadStream('resources/ElmDocument.icns').pipe(fs.createWriteStream('build/Elm/osx/Elm.app/Contents/Resources/file.icns'));
+  if (process.platform === 'darwin') {
+    exec('cd build/Elm/win; rm -f ../../Elm-win.zip; zip -r ../../Elm-win.zip *');
+    exec('cd build/Elm/osx; rm -f ../../Elm-mac.zip; zip -r ../../Elm-mac.zip Elm.app/*');
+  }
 }).catch(function(err) {
   console.error(err);
 });
